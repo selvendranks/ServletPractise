398237d8c6dd8fbec4c725f4ef5ee1c831dc6fb8
selvendranks <selvendranks@gmail.com>
2022-08-25 19:24:56 +0530

    added servlet and sent the first service request

---
+++ src/main/java/com/drag/DemoServlet.java
@@ -0,0 +1,22 @@
+package com.drag;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k=i+j;
+		
+		PrintWriter out = res.getWriter();
+		out.println("result is "+ k);
+	}
+}
